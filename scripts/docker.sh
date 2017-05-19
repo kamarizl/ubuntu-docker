0f70e13e2fa50c9be2d37336b064ee47d85fa57d
kamarizal <kamarizal@milkyway.local>
2017-05-19 23:52:08 +0800

    using ubuntu 16.04.2

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -29,6 +29,7 @@ apt-get install -y apt-transport-https ca-certificates curl software-properties-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
+apt-get -y upgrade
 apt-get install -y docker-ce
 
 echo "==> Cleaning up tmp"
@@ -70,35 +71,35 @@ let count--
 dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count
 rm /tmp/whitespace
 
-# Whiteout /boot
-count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
-let count--
-dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count
-rm /boot/whitespace
+## # Whiteout /boot
+## count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
+## let count--
+## dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count
+## rm /boot/whitespace
 
-echo '==> Clear out swap and disable until reboot'
-set +e
+## echo '==> Clear out swap and disable until reboot'
+## set +e
 
-swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
-case "$?" in
-    2|0) ;;
-    *) exit 1 ;;
-esac
-set -e
+## swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
+## case "$?" in
+##     2|0) ;;
+##     *) exit 1 ;;
+## esac
+## set -e
 
-if [ "x${swapuuid}" != "x" ]; then
-    # Whiteout the swap partition to reduce box size
-    # Swap is disabled till reboot
-    swappart=$(readlink -f /dev/disk/by-uuid/$swapuuid)
-    /sbin/swapoff "${swappart}"
-    dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
-    /sbin/mkswap -U "${swapuuid}" "${swappart}"
-fi
+## if [ "x${swapuuid}" != "x" ]; then
+##     # Whiteout the swap partition to reduce box size
+##     # Swap is disabled till reboot
+##     swappart=$(readlink -f /dev/disk/by-uuid/$swapuuid)
+##     /sbin/swapoff "${swappart}"
+##     dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
+##     /sbin/mkswap -U "${swapuuid}" "${swappart}"
+## fi
 
-# Zero out the free space to save space in the final image
-dd if=/dev/zero of=/EMPTY bs=1M  || echo "dd exit code $? is suppressed"
-rm -f /EMPTY
+## # Zero out the free space to save space in the final image
+## dd if=/dev/zero of=/EMPTY bs=1M  || echo "dd exit code $? is suppressed"
+## rm -f /EMPTY
 
-# Make sure we wait until all the data is written to disk, otherwise
-# Packer might quite too early before the large files are deleted
-sync
+## # Make sure we wait until all the data is written to disk, otherwise
+## # Packer might quite too early before the large files are deleted
+## sync
